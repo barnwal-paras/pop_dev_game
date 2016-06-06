701403975b5ec0cf39c11c0b9d48ca42adeb1a5d
barnwal-paras <parasbarnwal06@gmail.com>
2016-06-06 22:56:44 +0530

    added

---
+++ pop_dev/Assets/script/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	private Rigidbody2D rb;
+	private float drag;
+	public Vector2 force;
+	
+	void Start() {
+		
+		rb = GetComponent<Rigidbody2D> ();
+		rb.drag = drag;
+
+		rb.AddForce (force,ForceMode2D.Impulse);
+	}
+	void FixedUpdate() {
+		
+		rb.drag = drag;
+	}
+}
